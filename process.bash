b6b9a2baaf0dd75b85c53a78aab70b78131299ec
mjzbot <mjzbot@gmail.com>
2021-06-08 13:50:52 -0400

    auto

--- process.bash
+++ process.bash
@@ -2,7 +2,7 @@
 fileName=$(ls -1rt ~/Downloads/*.html | tail -n1)
 cp "$fileName" ~/repos/mjzbot.github.io/
 cd ~/repos/mjzbot.github.io/
-git add -A --quiet
+git add -A
 git commit -m "auto" --quiet
 git push myrepo --quiet
 pagename=$(echo "$fileName" | sed 's|.*/||')
