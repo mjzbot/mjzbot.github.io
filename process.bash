4b712e8db6e616a1bce84448279617d2c4f82a53
Jiaxing Zhang <jiaxingzhang@gmail.com>
2024-07-06 16:05:39 -0400

    added index.html

--- process.bash
+++ process.bash
@@ -2,7 +2,7 @@
 fileName=$(ls -1rt ~/Downloads/*.html | tail -n1)
 cp "$fileName" ~/repos/mjzbot.github.io/
 cd ~/repos/mjzbot.github.io/
-tree -H '.'     -L 1     --noreport    --houtro ""    --dirsfirst    --charset utf-8  --ignore-case --timefmt '%d-%b-%Y %H:%M' -I "index.html" -T 'Collections' -s -D -o index.html
+tree -H "." -L 1 --noreport --houtro "" --dirsfirst --charset utf-8 --ignore-case --timefmt "%d-%b-%Y %H:%M" -I "index.html" -T "Collections" -s -D -o index.html
 git add -A
 git commit -m "auto" --quiet
 git push myrepo --quiet
